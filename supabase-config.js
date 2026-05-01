f35ac9bc1adb1c1be361bd1cefd4192f83688a84
StomDevSage <juvenalgatsimbanyi@gmail.com>
2026-05-01 11:35:21 +0300

    all onset

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-// supabase-config.js - FIXED VERSION
-// No "const supabase" - we'll attach to window
-
-const SUPABASE_URL = 'https://yakusekcvidtvgcbbeap.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_s0CWHnbcqVTVHScSM4dMGQ_GYliBfEu';
-
-// Create supabase client and attach to window
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
